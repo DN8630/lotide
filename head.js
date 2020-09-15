ba0355ff2f4469f4bad4bc40148d4ca80359b13d
Daya <dayanayak30@gmail.com>
2020-09-15 00:08:16 +0000

    Head Added

--- head.js
+++ head.js
@@ -6,7 +6,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1,10);
+const head = function(array) {
+  return array[0];
+
+};
+
+assertEqual(head([5,6,7]),5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]),5);
+assertEqual(head([5]),5);
