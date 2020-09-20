fdee96e3d08d51579dd37b5f3e9215c159caf357
Daya <dayanayak30@gmail.com>
2020-09-20 03:53:42 +0000

    Refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,28 +1,10 @@
 // Function eqArray to compare two arrays.
-const eqArray = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  } else {
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] !== secondArray[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
+const eqArray = require("./eqArrays");
 const assertArrayEqual = function(firstArr,secondArr) {
   if (eqArray(firstArr,secondArr) === true) {
     console.log(`✅✅ Assertion Passed! [${firstArr}] === [${[secondArr]}]`);
-  }
-  else {
+  } else {
     console.log(`🔴🔴 Assertion Failed! [${firstArr}] !== [${secondArr}]`);
   }
 };
-
-assertArrayEqual([1,2,3],[1,2,3]);
-assertArrayEqual([1,2],[]);
-assertArrayEqual([1,2,3,4],[1,2,4,3]);
-assertArrayEqual(["Hello","Good"],["Hello","Good"]);
-assertArrayEqual(["Hello","Good"],["Hello","Bad"]);
+module.exports = assertArrayEqual;
